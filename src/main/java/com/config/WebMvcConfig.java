dbaf90efa9a6a9dc7c3cdc1034563a5f2f32e5d5
dh <hyunlove12@naver.com>
2020-12-26 13:59:52 +0900

    최종

--- src/main/java/com/config/WebMvcConfig.java
+++ src/main/java/com/config/WebMvcConfig.java
@@ -2,14 +2,17 @@ package com.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
@@ -18,6 +21,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 			"classpath:/static/"
 	};
 	
+	@Bean 
+	public ServletRegistrationBean servletRegistrationBean(WebApplicationContext webApplicationContext) {
+		  DispatcherServlet disServlet = new DispatcherServlet(webApplicationContext);
+		  ServletRegistrationBean servlet = new ServletRegistrationBean(disServlet, false, "/"); 
+		  servlet.setLoadOnStartup(1); 
+		  return servlet; 
+	}
 	@Autowired
 	@Qualifier(value = "loginInterceptor")
 	private HandlerInterceptor interceptor;
